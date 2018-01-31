59756d8976bd58e3fd37a37e90eafabe1e4c7701
Easy <240018840@qq.com>
2018-01-31 17:02:59 +0800

    .sh

---
+++ src/main/resources/shutdown.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+echo "Shutdown..."
+
+#JAR path
+JAR_MAIN=spidertask.jar
+
+#Get PID
+pid=0
+
+getPid(){
+    javaps='jps -l | grep $JAR_MAIN'
+    if [ -n "$javaps" ]; then
+        pid=$(jps -l | grep $JAR_MAIN | awk '{print $1}')
+    else
+        pid=0
+    fi
+}
+
+#Check to see if there is a started service
+shutdown(){
+    getPid
+    if [ -n "$pid" ]; then
+        echo "Find started service... $pid"
+        echo "Kill $pid"
+        kill -9 $pid
+        echo "Service stopped..."
+    else
+        echo "Can not find started service..."
+    fi
+}
+shutdown
---
+++ src/main/resources/startup.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+echo "Startup..."
+
+#JAR path
+JAR_MAIN=spidertask.jar
+
+#Get PID
+pid=0
+
+getPid(){
+    javaps='jps -l | grep $JAR_MAIN'
+    if [ -n "$javaps" ]; then
+        pid=$(jps -l | grep $JAR_MAIN | awk '{print $1}')
+    else
+        pid=0
+    fi
+}
+
+#Check to see if there is a started service
+startup(){
+    getPid
+    if [ -n "$pid" ]; then
+        echo "Find started service... $pid"
+    else    
+        exec java -server -jar  $JAR_MAIN &
+        getPid
+        echo "Service started ... $pid"
+    fi
+}
+startup
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
